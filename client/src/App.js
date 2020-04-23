8ae8295124f012361a7cd0be252aa6ed3bea73a0
Andrew KIl <kil.andrew@gmail.com>
2020-04-22 22:40:33 -0400

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AddLogForm from "./components/AddLogForm";
 import "./App.css";
 
 const App = () => (
   <div className="App">
-    <h1>Sleep Journal</h1>
+    <h1>Sleep Tracker</h1>
+    <AddLogForm></AddLogForm>
   </div>
 );
 
