0c56e045dcdc424de5b26fc21a3083cd4d80fddd
Andrew KIl <kil.andrew@gmail.com>
2020-04-25 19:46:52 -0400

    add AddSleepLogForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import AddLogForm from "./components/AddLogForm";
+import AddSleepLogForm from "./components/AddSleepLogForm";
 import "./App.css";
 
 const App = () => (
   <div className="App">
     <h1>Sleep Tracker</h1>
-    <AddLogForm></AddLogForm>
+    <AddSleepLogForm></AddSleepLogForm>
   </div>
 );
 
---
+++ client/src/components/AddSleepLogForm.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const AddSleepLogForm = () => {
+  const INITIAL_STATE = { notes: "" };
+
+  const [values, setValues] = useState(INITIAL_STATE);
+
+  const handleChange = (e) =>
+    setValues({ ...values, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => e.preventDefault();
+
+  useEffect(() => console.log(values));
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="notes"
+        placeholder="Add notes here"
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default AddSleepLogForm;
