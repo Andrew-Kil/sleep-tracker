deba36a8778a853488256cfe85713e06d603b3af
Andrew KIl <kil.andrew@gmail.com>
2020-04-26 18:44:25 -0400

    add loading to components

--- client/src/components/DisplaySleepLogs.js
+++ client/src/components/DisplaySleepLogs.js
@@ -3,11 +3,14 @@ import axios from "axios";
 
 const DisplaySleepLogs = () => {
   const [sleepLogs, setSleepLogs] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const result = await axios("http://localhost:5000/sleep-logs");
       setSleepLogs(result.data);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
@@ -15,7 +18,10 @@ const DisplaySleepLogs = () => {
   return (
     <div>
       <h2>Sleep Logs</h2>
-      {sleepLogs &&
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        sleepLogs &&
         sleepLogs.data.map((sleepLog, i) => (
           <div style={{ border: "1px solid black", margin: "10px" }} key={i}>
             <div style={{ marginTop: "20px" }}>user id: {sleepLog.user_id}</div>
@@ -34,7 +40,8 @@ const DisplaySleepLogs = () => {
             </div>
             <div style={{ marginTop: "20px" }}>notes: {sleepLog.notes}</div>
           </div>
-        ))}
+        ))
+      )}
     </div>
   );
 };
