6022a37657937ec87114912cc3e7f158f682b911
Andrew KIl <kil.andrew@gmail.com>
2020-04-26 19:45:03 -0400

    add NavBar, Switch, Route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+
+import { Switch, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
 import AddSleepLogForm from "./components/AddSleepLogForm";
 import DisplaySleepLogs from "./components/DisplaySleepLogs";
 import DisplayDreamThemes from "./components/DisplayDreamThemes";
+
 import "./App.css";
 
 const App = () => (
   <div className="App">
+    <NavBar></NavBar>
     <h1>Sleep Tracker</h1>
     <AddSleepLogForm></AddSleepLogForm>
-    <DisplaySleepLogs></DisplaySleepLogs>
-    <DisplayDreamThemes></DisplayDreamThemes>
+
+    <Switch>
+      <Route exact path="/sleep-logs" component={DisplaySleepLogs}></Route>
+      <Route exact path="/dream-themes" component={DisplayDreamThemes}></Route>
+    </Switch>
   </div>
 );
 
