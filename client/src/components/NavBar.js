de678f37b3bd4ef289ce108541e72c2c7e00de53
Andrew KIl <kil.andrew@gmail.com>
2020-04-26 19:45:45 -0400

    add NavBar component

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => (
+  <div>
+    <nav>
+      <span>
+        <NavLink to="/">
+          <span role="img" aria-label="sleep emoji">
+            😴
+          </span>
+        </NavLink>
+      </span>
+      <div>
+        <NavLink to="sleep-logs">Sleep Logs</NavLink>
+      </div>
+      <div>
+        <NavLink to="dream-themes">Dream Themes</NavLink>
+      </div>
+    </nav>
+  </div>
+);
+export default NavBar;
