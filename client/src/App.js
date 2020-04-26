edf3994a50a12533d0c06e8b126f6ab46a21c75e
Andrew KIl <kil.andrew@gmail.com>
2020-04-26 12:00:25 -0400

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AddSleepLogForm from "./components/AddSleepLogForm";
+import DisplaySleepLogs from "./components/DisplaySleepLogs";
 import "./App.css";
 
 const App = () => (
   <div className="App">
     <h1>Sleep Tracker</h1>
     <AddSleepLogForm></AddSleepLogForm>
+    <DisplaySleepLogs></DisplaySleepLogs>
   </div>
 );
 
