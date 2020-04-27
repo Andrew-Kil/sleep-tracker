8f7dcaa08b9ac1961e89bc05eff76a5f1d430b35
Andrew KIl <kil.andrew@gmail.com>
2020-04-27 15:47:34 -0400

    update AddSleepLogForm

---
+++ client/src/components/SleepLogs/Form/AddSleepLogForm.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+import Form from "./Form";
+
+const AddSleepLogForm = () => {
+  // const INITIAL_STATE = { notes: "" };
+
+  // const [values, setValues] = useState(INITIAL_STATE);
+
+  // const handleChange = (e) =>
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+
+  // const handleSubmit = (e) => e.preventDefault();
+
+  // useEffect(() => console.log(values));
+
+  return <Form></Form>;
+};
+
+export default AddSleepLogForm;
