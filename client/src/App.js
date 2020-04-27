7829fa1da0d1c80535f7658fd309ab1598ba9187
Andrew KIl <kil.andrew@gmail.com>
2020-04-27 16:01:30 -0400

    add Home component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 import SleepLogs from "./components/SleepLogs/SleepLogs";
 import DisplayDreamThemes from "./components/DisplayDreamThemes";
 
@@ -15,6 +16,7 @@ const App = () => (
     <h1>Sleep Tracker</h1>
 
     <Switch>
+      <Route exact path="/" component={Home}></Route>
       <Route exact path="/sleep-logs" component={SleepLogs}></Route>
       <Route exact path="/dream-themes" component={DisplayDreamThemes}></Route>
     </Switch>
