fa997b51d858d9ed91e7f45e320e87bf391f58ac
Andrew KIl <kil.andrew@gmail.com>
2020-04-27 13:50:16 -0400

    add CssBaseline and MuiThemeProvider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createMuiTheme();
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
