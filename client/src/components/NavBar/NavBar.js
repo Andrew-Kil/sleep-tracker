fec5fc04856fa12a580595b06c7a0de75330a8b6
Andrew KIl <kil.andrew@gmail.com>
2020-04-28 10:26:12 -0400

    update NavBar

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+  Fab,
+  Zoom,
+  Paper,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+
+import { NavLink } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+const ScrollTop = (props) => {
+  const { children, window } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+};
+
+const NavBar = (props) => {
+  const classes = useStyles();
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <AppBar>
+        <Toolbar>
+          <Grid container spacing={6}>
+            <Grid item xs>
+              <Paper className={classes.paper}>
+                <NavLink to="/" style={{ textDecoration: "none" }}>
+                  <Typography variant="h6">Home</Typography>
+                </NavLink>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className={classes.paper}>
+                <NavLink to="sleep-logs" style={{ textDecoration: "none" }}>
+                  <Typography variant="h6">Sleep Logs</Typography>
+                </NavLink>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className={classes.paper}>
+                <NavLink to="dream-themes" style={{ textDecoration: "none" }}>
+                  <Typography variant="h6">Dream Themes</Typography>
+                </NavLink>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <ScrollTop {...props}>
+        <Fab color="primary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </div>
+  );
+};
+
+export default NavBar;
