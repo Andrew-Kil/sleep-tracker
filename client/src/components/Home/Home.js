f5d4ade6ea367e58eb406b13b8ca81f5ddaf0510
Andrew KIl <kil.andrew@gmail.com>
2020-04-28 11:59:44 -0400

    add png

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import sloth from "../../assets/sloth.png";
+
 const Home = () => (
   <>
     <h1>Sleep Tracker</h1>
-    <div>Home</div>
+    <img src={sloth} alt="" />
   </>
 );
 
