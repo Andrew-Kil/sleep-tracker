105efccbe96853612458a85052532c675a8516b4
Andrew KIl <kil.andrew@gmail.com>
2020-05-03 22:03:37 -0400

    update helpers.js

--- server/auth/helpers.js
+++ server/auth/helpers.js
@@ -1,25 +1,51 @@
 const bcrypt = require("bcryptjs");
 
-function comparePass(userPass, databasePass) {
-  return bcrypt.compareSync(userPass, databasePass);
-}
-
-function createHash(password) {
-  const salt = bcrypt.genSaltSync();
-  const hash = bcrypt.hashSync(password, salt);
-  return hash;
-}
-
-function loginRequired(req, res, next) {
-  if (!req.user) {
-    res.status(401).json({ status: "Forbidden - please log in." });
-    return;
+const hashPassword = async (plainPassword) => {
+  try {
+    const passwordDigest = await bcrypt.hash(plainPassword, 10);
+    return passwordDigest;
+  } catch (err) {
+    throw err;
   }
-  next();
-}
+};
+
+const comparePasswords = (plainPassword, passwordDigest) => {
+  return bcrypt.compare(plainPassword, passwordDigest);
+};
+
+const loginRequired = (req, res, next) => {
+  if (req.user) return next();
+  res.status(401).json({
+    payload: {
+      message: "Unauthorized",
+    },
+    error: true,
+  });
+};
+
+const logoutUser = (req, res) => {
+  req.logout();
+  res.status(200).send("log out success");
+};
+
+const isLoggedIn = (req, res) => {
+  if (req.user) {
+    res.json({
+      username: req.user,
+      msg: "User logged in",
+    });
+  } else {
+    res.json({
+      username: null,
+      msg: "User not logged in",
+    });
+  }
+};
 
 module.exports = {
-  comparePass,
-  createHash,
+  hashPassword,
+  comparePasswords,
   loginRequired,
+  logoutUser,
+  isLoggedIn,
 };
