6e2aa6574e6ef96ab1b7b5cd77bc154b7a5364c8
Andrew KIl <kil.andrew@gmail.com>
2020-05-04 19:36:23 -0400

    update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const usersQueries = require("../db/queries/users");
+// const usersQueries = require("../db/queries/users");
+const db = require("../db/queries/auth");
+const passport = require("../auth/local");
+const { loginRequired } = require("../auth/helpers");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const users = await usersQueries.getAllUsers();
-    res.send({
-      payload: users,
-      msg: "Retrieved all users",
-      err: false,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get("/", async (req, res, next) => {
+//   try {
+//     const users = await usersQueries.getAllUsers();
+//     res.send({
+//       payload: users,
+//       msg: "Retrieved all users",
+//       err: false,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.get("/", loginRequired, db.getUsers);
+router.post("/new", db.createUser);
+router.post("/login", passport.authenticate("local", {}), db.loginUser);
+router.get("/isLoggedIn", db.isLoggedIn);
+router.post("/logout", loginRequired, db.logoutUser);
 
 module.exports = router;
