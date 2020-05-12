1f3f7126793993058dfc3d9d636b9a7ecdc661ed
Andrew KIl <kil.andrew@gmail.com>
2020-05-12 13:17:00 -0400

    update href

--- client/src/components/Auth/Copyright.js
+++ client/src/components/Auth/Copyright.js
@@ -7,7 +7,7 @@ const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="/">
         Sleep Tracker
       </Link>{" "}
       {new Date().getFullYear()}
