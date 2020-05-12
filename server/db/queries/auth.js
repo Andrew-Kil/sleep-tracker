efba6f49935f13f15ae40f48f87534231ee6fb39
Andrew KIl <kil.andrew@gmail.com>
2020-05-12 10:35:58 -0400

    fix auth queries

--- server/db/queries/auth.js
+++ server/db/queries/auth.js
@@ -1,25 +1,22 @@
-const db = require("../index");
+const { db } = require("../index");
 
-const authHelpers = require("../../auth/helpers");
-
-function createUser(req, res, next) {
-  const hash = authHelpers.hashPassword(req.body.password);
-
-  db.one(
-    "INSERT INTO users (username, password_digest) VALUES (${username}, ${password})",
-    { username: req.body.username, password: hash }
-  )
-    .then(() => {
-      res.status(200).json({
-        message: "Registration successful.",
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: err,
-      });
+const createUser = async (req, res, next) => {
+  try {
+    const { firebase_id, email } = req.body;
+    await db.one(
+      "INSERT INTO users (firebase_id, email) VALUES (${firebase_id}, ${email}) RETURNING *",
+      {
+        firebase_id,
+        email,
+      }
+    );
+    res.json({
+      message: "Created new user",
     });
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
 function logoutUser(req, res, next) {
   req.logout();
