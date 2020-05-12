2365df15f86d36c1005a854591f4da955f1374e3
Andrew KIl <kil.andrew@gmail.com>
2020-05-12 13:16:48 -0400

    restructure App and Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,42 +1,16 @@
-import React, { useEffect, useState } from "react";
-import firebase from "../../firebase";
-
+import React from "react";
 import sloth from "../../assets/sloth.png";
 
-const Home = () => {
-  let [userMeta, setUserMeta] = useState({});
-  let [token, setToken] = useState({});
-
-  useEffect(() => {
-    const stopListening = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        const { email, uid } = user;
-        const lastLogin = user.metadata.lastSignInTime;
-
-        setUserMeta({ email, uid, lastLogin });
-      } else {
-        console.log("user is not logged in");
-      }
-    });
-    return () => {
-      stopListening();
-    };
-  }, []);
-
+const Home = (props) => {
   return (
     <>
       <h1>Sleep Tracker</h1>
-      {!userMeta.email ? (
-        <h3> Please log in </h3>
+      {!props.userMeta.email ? (
+        <h3>Please log in</h3>
       ) : (
         <>
-          <h2>Welcome, {userMeta.email}</h2>
-          {/* <h4>Your ID is: {userMeta.uid}</h4>
-          <h4>You last signed in: {userMeta.lastLogin}</h4>
-          <button onClick={handleUnprotectedAPI}>
-            Unprotected API Invokation
-          </button>
-          <button onClick={handleProtectedAPI}>Protected API</button> */}
+          <h2>Welcome, {props.userMeta.email}</h2>
+          <h4>You last signed in: {props.userMeta.lastLogin}</h4>
         </>
       )}
       <img src={sloth} alt="" />
