b6a0616f67713c1c64a9a73797ad3626764b71ea
Andrew KIl <kil.andrew@gmail.com>
2020-05-13 16:51:16 -0400

    clean up app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,12 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const session = require("express-session");
-const passport = require("passport");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const sleepLogsRouter = require("./routes/sleepLogs");
 const dreamThemesRouter = require("./routes/dreamThemes");
-const authRouter = require("./routes/auth");
 
 const app = express();
 
@@ -26,25 +22,12 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("SUPER_SECRET_SECRET"));
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(
-  session({
-    secret: "SUPER_SECRET_SECRET",
-    saveUninitialized: true,
-    resave: true,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/sleep-logs", sleepLogsRouter);
 app.use("/dream-themes", dreamThemesRouter);
-app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
