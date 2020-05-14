7b310bdeea08efdb5dc20fe8b27cdbb788d67a78
Andrew KIl <kil.andrew@gmail.com>
2020-05-14 13:39:29 -0400

    add PublicSleepLogs component

---
+++ client/src/components/SleepLogs/PublicSleepLogs.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}));
+
+const PublicSleepLogs = () => {
+  const [publicSleepLogs, setPublicSleepLogs] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const classes = useStyles();
+
+  const convertISODate = (postDate) => {
+    const date = postDate.substring(0, 10);
+    const month = date.split("-")[1];
+    const day = date.split("-")[2];
+    const year = date.split("-")[0];
+    return `${month}-${day}-${year}`;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const result = await axios("http://localhost:5000/sleep-logs/public");
+      setPublicSleepLogs(result.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Public Sleep Logs</h2>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        publicSleepLogs &&
+        publicSleepLogs.data
+          .sort((a, b) => {
+            const c = convertISODate(a.post_date);
+            const d = convertISODate(b.post_date);
+            return c < d ? 1 : c > d ? -1 : 0;
+          })
+          .map((publicSleepLog, i) => (
+            <div key={i}>
+              <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center">
+                <Grid item xs={6} style={{ minWidth: "75vw", margin: "25px" }}>
+                  <Paper elevation={3} className={classes.root} key={i}>
+                    <Typography variant="h5" component="h3">
+                      {publicSleepLog.post_date.slice(0, 10)}
+                    </Typography>
+                    <Typography component="p">
+                      remember dream:{" "}
+                      {publicSleepLog.remember_dream ? "true" : "false"}
+                    </Typography>
+                    <Typography component="p">
+                      interrupted sleep:{" "}
+                      {publicSleepLog.interrupted_sleep ? "true" : "false"}
+                    </Typography>
+                    <Typography component="p">
+                      sleep start: {publicSleepLog.sleep_start}
+                    </Typography>
+                    <Typography component="p">
+                      sleep end:{publicSleepLog.sleep_end}
+                    </Typography>
+                    <Typography component="p">
+                      notes: {publicSleepLog.notes}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </div>
+          ))
+      )}
+    </div>
+  );
+};
+
+export default PublicSleepLogs;
