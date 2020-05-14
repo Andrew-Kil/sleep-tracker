38640556d8d40ac0700fcbdf331bf0591f494b88
Andrew KIl <kil.andrew@gmail.com>
2020-05-14 13:39:18 -0400

    add button to direct user to public sleep logs

--- client/src/components/SleepLogs/SleepLogs.js
+++ client/src/components/SleepLogs/SleepLogs.js
@@ -1,13 +1,19 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 import SleepLogsForm from "./SleepLogsForm";
 import DisplaySleepLogs from "./DisplaySleepLogs";
+import { Button } from "@material-ui/core";
 
-const SleepLogs = (props) => (
-  <>
-    <SleepLogsForm userMeta={props.userMeta}></SleepLogsForm>
-    <DisplaySleepLogs userMeta={props.userMeta}></DisplaySleepLogs>
-  </>
-);
+const SleepLogs = (props) => {
+  return (
+    <>
+      <Button>
+        <NavLink to="/sleep-logs/public"> View public sleep logs</NavLink>
+      </Button>
+      <SleepLogsForm userMeta={props.userMeta}></SleepLogsForm>
+      <DisplaySleepLogs userMeta={props.userMeta}></DisplaySleepLogs>
+    </>
+  );
+};
 
 export default SleepLogs;
