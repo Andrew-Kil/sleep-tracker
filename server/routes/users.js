5f05f08bcfc9f4b7a24598c14ded41b9f057d1c3
Andrew KIl <kil.andrew@gmail.com>
2020-05-16 15:34:45 -0400

    add updateUser route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, getUserByFirebaseID } = require("../db/queries/users");
+const {
+  createUser,
+  updateUser,
+  getUserByFirebaseID,
+} = require("../db/queries/users");
 
 router.post("/new", createUser);
+router.patch("/profile/:id", updateUser);
 router.get("/profile/:id", getUserByFirebaseID);
 
 module.exports = router;
