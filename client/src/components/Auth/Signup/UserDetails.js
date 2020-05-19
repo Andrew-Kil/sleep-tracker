0695aa8f6b5975353f8910ece1712969478f361b
Andrew KIl <kil.andrew@gmail.com>
2020-05-18 23:46:09 -0400

    add UserDetails component

---
+++ client/src/components/Auth/Signup/UserDetails.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+import {
+  Avatar,
+  Button,
+  TextField,
+  Link,
+  Grid,
+  Typography,
+  Container,
+  makeStyles,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const UserDetails = ({
+  form: { email, password },
+  incrementStep,
+  handleField,
+}) => {
+  const classes = useStyles();
+
+  const nextStep = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      alert("Please enter an email address and password");
+    } else {
+      incrementStep();
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h5" variant="h5">
+          Sign up
+        </Typography>
+        <Typography component="p" variant="p">
+          User Details
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={(e) => nextStep(e)}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => handleField(e)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => handleField(e)}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            Next
+          </Button>
+          <Grid container justify="center">
+            <Grid item>
+              <Link href="/auth/login" variant="body2">
+                Already have an account? Log in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default UserDetails;
