9c34d47ebe5b89785076d36c6d619731e016df38
Andrew KIl <kil.andrew@gmail.com>
2020-05-20 14:48:47 -0400

    update getAllPublicSleepLogs query to get first name and last name

--- server/db/queries/sleepLogs.js
+++ server/db/queries/sleepLogs.js
@@ -13,7 +13,9 @@ const getAllSleepLogs = (req, res, next) => {
 };
 
 const getAllPublicSleepLogs = (req, res, next) => {
-  db.any("SELECT * FROM sleep_logs WHERE is_private = false")
+  db.any(
+    "SELECT users.first_name, users.last_name, sleep_logs.post_date, sleep_logs.remember_dream, sleep_logs.sleep_interrupted, sleep_logs.sleep_start, sleep_logs.sleep_end, sleep_logs.notes FROM sleep_logs JOIN users ON users.firebase_id = sleep_logs.user_id WHERE is_private = FALSE"
+  )
     .then((data) => {
       res.status(200).json({
         status: "Success",
